b7a58075d00d4e8e73f45a5a645bc7be6558fea6
livewt <72449349+livewt@users.noreply.github.com>
2022-02-01 14:22:11 +0100

    Login to subscription on DN

--- thesiscode.r
+++ thesiscode.r
@@ -1,30 +1,45 @@
-rm(list = ls())
+
 # R CODE MASTER THESIS:
-require(rvest)  # to get wikipedia info about companies+tickers
-require(BatchGetSymbols)  # to get stock prices from yahoo
+
+# Libraries
+library(rvest)
+
+# Read in stock data
+OSB <- read.csv("OSB.csv", sep = ";")
+
+# Log in to DN subcription
+url <- "https://www.dn.no/auth/login"
+uastring <- "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+session <- session(url, user_agent(uastring))
+form <- html_form(session)[[1]]
+fill <- html_form_set(form, 
+                             username = "livewt@live.no",
+                             password = "masterthesis123")
+session_submit(session, fill, submit = NULL, config(referer = session$url))
+
+# Jump to page with news articles 
+jump <- session %>% session_jump_to("https://www.dn.no/sok/?topics=Finans%2CPrivat%25C3%25B8konomi%2CB%25C3%25B8rs%2COslo%2520B%25C3%25B8rs%2CMakro%25C3%25B8konomi%2C%25C3%2598konomi&date=[01.01.2010-31.12.2019]&fbclid=IwAR24oNsM3yfEJb-jpAQXVNkg3qOLVT2QK7HfXX9_exZBEjUG43cslyR1anE") 
+follow <- session %>% session_follow_link(css = "p a")
 
 
-# List of OBX companies (from June 2020, maybe we will edit this manually to reflect 2019 info?)
-url <- "https://en.wikipedia.org/wiki/OBX_Index" 
-firms <- as.data.frame(read_html(url) %>% 
-                       html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[1]') %>% #table containing the information
-                       html_table()) #retrieve table
+# Retrieve dates and text
 
-# Create vector of tickers
-tickers.obx = as.vector(paste0(firms$Ticker.symbol,".OL"))
 
-# Rename one wrong ticker
-tickers.obx = gsub("AKERBP.OL","AKRBP.OL",tickers.obx)
 
-#Using tickers vector to obtain stock data from Yahoo Finance
-obx.stocks <- BatchGetSymbols(tickers = tickers.obx,
-                              first.date = "2010-01-01",
-                              last.date = "2019-12-31",
-                              freq.data = "daily",
-                              do.cache = FALSE
-)
 
-# How many rows do we have?
-# We originally had 25 tickers
-print(obx.stocks$df.control)
 
+
+# Example from exam
+for (year in years) {
+  base.url <- "https://www.federalreserve.gov"
+  web.url <- paste(base.url, "/monetarypolicy/fomchistorical",
+                   year, ".htm", sep ="")
+  html <- read_html(web.url)
+  raw_list <- html %>% # takes the page above for which we've read the html
+    html_nodes("a") %>%  # find all links in the page
+    html_attr("href") %>% # get the url for these links
+    str_subset("\\meeting.pdf") %>% # find those that end in meeting.pdf only
+    str_c(base.url, .) # prepend the website to the url
+  
+  pdfs <- append(pdfs, raw_list)
+}
