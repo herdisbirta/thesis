1343a4011523a4b1ec725e51f9a66192da7ef7e8
herdisbirta <herdisbirta1@gmail.com>
2022-02-06 16:54:02 +0100

    Identified all wrong tickers in stock prices, rearranged code

--- thesiscode.r
+++ thesiscode.r
@@ -12,6 +12,66 @@ library(BatchGetSymbols)
 library(lexicon)
 library(stopwords)
 
+
+# STOCK PRICE RETRIEVAL
+
+# List of OBX companies (from June 2020, maybe we will edit this manually to reflect 2019 info?)
+url = "https://en.wikipedia.org/wiki/OBX_Index" 
+firms = as.data.frame(read_html(url) %>% 
+                        html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[1]') %>% #table containing the information
+                        html_table()) #retrieve table
+
+# Manually go over and change wrong tickers,
+firms$ticker = 
+  firms$Ticker.symbol %>% 
+  gsub("AKERBP","AKRBP",.) %>% 
+  gsub("SSO","SCATC",.) %>% 
+  gsub("SCH","SCHA",.)
+  
+  
+# add ".OL" to each ticker so it can be retrieved later
+firms$ticker = paste0(firms$ticker,".OL")
+
+# Remove unneccessary information from data frame
+firms = select(firms,"company" = Company,ticker)
+
+# Change the company names so they match with how they are most frequently referred to
+# (example: "Yara International" is most often referred to as simply "Yara")
+firms$company = gsub("Yara International", "Yara",firms$company)
+# MORE EXAMPLES OF THIS?
+
+
+# Create vector of tickers
+tickers.obx = as.vector(firms$ticker)
+
+#Using tickers vector to obtain stock data from Yahoo Finance
+obx.stocks = BatchGetSymbols(tickers = tickers.obx,
+                             first.date = "2010-01-01",
+                             last.date = "2019-12-31",
+                             freq.data = "daily",
+                             do.cache = FALSE,
+                             thresh.bad.data = 0 # Only skip ticker if no data
+)
+
+# Check number of rows
+# We  had 25 tickers and we have 25 rows
+print(obx.stocks$df.control)
+
+# Convert stock information into a data frame
+stocks = obx.stocks$df.tickers
+
+# Add company name (for searching purposes maybe?)
+stocks = left_join(stocks,firms,by="ticker")
+
+# Keep only the information we will use (ticker, date, opening+closing price)
+stocks = select(stocks,company,ticker,ref.date,price.open,price.close)
+
+# Calculate a daily price measure
+for(i in 1:length(stocks)){
+  stocks$av.price[i] = (stocks$price.open[i]+stocks$price.close[i])/2
+}
+
+
 # Extract URLs and dates for each article
 articles <- 1:20
 html <- read_html("DN.html")  # HTML code from DN
@@ -75,55 +135,6 @@ rm(list = ls())
 load("data.Rdata")
 
 
-# STOCK PRICE RETRIEVAL
-
-# List of OBX companies (from June 2020, maybe we will edit this manually to reflect 2019 info?)
-url = "https://en.wikipedia.org/wiki/OBX_Index" 
-firms = as.data.frame(read_html(url) %>% 
-                       html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[1]') %>% #table containing the information
-                       html_table()) #retrieve table
-
-# Rename one wrong ticker, add ".OL" to each ticker so it can be retrieved later
-firms$ticker = paste0(gsub("AKERBP","AKRBP",firms$Ticker.symbol),".OL")
-
-# Remove unneccessary information from data frame
-firms = select(firms,"company" = Company,ticker)
-
-# Change the company names so they match with how they are most frequently referred to
-# (example: "Yara International" is most often referred to as simply "Yara")
-firms$company = gsub("Yara International", "Yara",firms$company)
-# MORE EXAMPLES OF THIS?
-
-
-# Create vector of tickers
-tickers.obx = as.vector(firms$ticker)
-
-#Using tickers vector to obtain stock data from Yahoo Finance
-obx.stocks = BatchGetSymbols(tickers = tickers.obx,
-                              first.date = "2010-01-01",
-                              last.date = "2019-12-31",
-                              freq.data = "daily",
-                              do.cache = FALSE,
-                              thresh.bad.data = 0 # Only skip ticker if no data
-)
-
-# Check number of rows
-# We originally had 25 tickers, now we have 23 rows
-print(obx.stocks$df.control)
-
-# Convert stock information into a data frame
-stocks = obx.stocks$df.tickers
-
-# Add company name (for searching purposes maybe?)
-stocks = left_join(stocks,firms,by="ticker")
-
-# Keep only the information we will use (ticker, date, opening+closing price)
-stocks = select(stocks,company,ticker,ref.date,price.open,price.close)
-
-# Calculate a daily price measure
-for(i in 1:length(stocks)){
-  stocks$av.price[i] = (stocks$price.open[i]+stocks$price.close[i])/2
-}
 
 
 
