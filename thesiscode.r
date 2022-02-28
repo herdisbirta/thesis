d055313fcda3872724d6a77e147a4d3fdca38746
herdisbirta <herdisbirta1@gmail.com>
2022-02-28 12:41:19 +0100

    Squashed commit of the following:
    commit 52d9fd7895e7bcd01598658bbed45b1554d73bd5
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Fri Feb 25 19:27:33 2022 +0100
    
        Combined mine and your changes from today :)
    
    commit 4e803a881f3441af8145f3fd6c1f7e18dc61d506
    Merge: b8de34c 7e9ee8b
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Fri Feb 25 19:21:48 2022 +0100
    
        Merge branch 'searches' of https://github.com/herdisbirta/thesis into searches
    
    commit b8de34c359f0d784262498dd3259a54963903be3
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Fri Feb 25 19:18:11 2022 +0100
    
        Fixed loop choosing what company is mentioned most
    
    commit 7e9ee8bf06b728afab657028c67c594cd67a3ec2
    Merge: 302157e 4b56d1a
    Author: herdisbirta <herdisbirta1@gmail.com>
    Date:   Fri Feb 25 18:57:26 2022 +0100
    
        Merge branch 'searches' of https://github.com/herdisbirta/thesis into searches
    
    commit 302157eafbdeb6655f4edd999fd18090c7d28a7e
    Author: herdisbirta <herdisbirta1@gmail.com>
    Date:   Fri Feb 25 18:47:31 2022 +0100
    
        Combined articles for same company on same day
    
    commit 4b56d1ad9f34674af436702e37d46aa37f2f6498
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Fri Feb 25 14:40:11 2022 +0100
    
        Added company names changed during time period
    
    commit ea1eb9e0080c5a28ee7e6019233f45b6f070a204
    Author: herdisbirta <herdisbirta1@gmail.com>
    Date:   Thu Feb 24 23:48:12 2022 +0100
    
        Fixed mistake in stock prices
    
    commit 138506555de5a1f4101f66adcb3b3a8f983c6c15
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Thu Feb 24 18:36:27 2022 +0100
    
        Was just for testing something
    
    commit 3e532a20038aaf8ca114c33c0358a02713a3fe03
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Thu Feb 24 18:36:16 2022 +0100
    
        Was just for testing something
    
    commit 7b1b6d40d095e91faee63fb995fcd7d92db04cdf
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Thu Feb 24 18:35:50 2022 +0100
    
        Matching articles to companies
    
    commit fcac0c21a48d923ae95cc2b1849ed2410653c6c2
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Wed Feb 23 22:25:18 2022 +0100
    
        Loop to see how many times each company is mentioned per article
    
        (Not finished)
    
    commit 4c1fce1c6f2108840908d8975b740454170aa8f7
    Merge: 5e603c3 5fa8fbb
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Wed Feb 23 19:47:55 2022 +0100
    
        Merge branch 'searches' of https://github.com/herdisbirta/thesis into searches
    
    commit 5e603c30070fefe7db56661700a7c8f8350b45ab
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Wed Feb 23 19:42:01 2022 +0100
    
        Because Rstudio is a retard
    
    commit 5fa8fbb5eb2e298a76b5c044b4b2509b86bbda46
    Author: herdisbirta <herdisbirta1@gmail.com>
    Date:   Wed Feb 23 18:36:40 2022 +0100
    
        Finalized company list, added Statoil
    
    commit 565e68b31af778c0e23f8ec38619fdbeaea79d4c
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Tue Feb 22 18:33:44 2022 +0100
    
        Loop to find articles with company names
    
        Still needs a bit of work
    
    commit 3066582c11cab0f308119b6b5df0b25db6928427
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Mon Feb 21 18:33:11 2022 +0100
    
        just trying something
    
    commit f84461313eed4ea84684fa54fa4c6b6199cbae9d
    Author: livewt <72449349+livewt@users.noreply.github.com>
    Date:   Mon Feb 21 17:11:05 2022 +0100
    
        Change in mentions
    
    commit 76fb3fc07c9970ffe1daf8d685c55d7c8f397063
    Author: herdisbirta <herdisbirta1@gmail.com>
    Date:   Mon Feb 21 00:48:42 2022 +0100
    
        Added more comments and tests
    
    commit 579b1334e1aec7e32007b8037cc1f9c294928659
    Author: herdisbirta <herdisbirta1@gmail.com>
    Date:   Mon Feb 21 00:37:01 2022 +0100
    
        Added comments on code
    
    commit 0b129e57827525de738f9aa6d83f370e4d62ce29
    Author: herdisbirta <herdisbirta1@gmail.com>
    Date:   Sun Feb 20 16:33:43 2022 +0100
    
        Remove companies that are never mentioned, find number of mentions per article

--- thesiscode.r
+++ thesiscode.r
@@ -18,8 +18,7 @@ library(dplyr)
 # STOCK PRICE RETRIEVAL
 
 # List of ALL listed companies
-url <- "https://en.wikipedia.org/wiki/List_of_companies_listed_on_the_Oslo_Stock_Exchange"
-all.firms <- as.data.frame(read_html(url) %>% 
+all.firms <- as.data.frame(read_html("https://en.wikipedia.org/wiki/List_of_companies_listed_on_the_Oslo_Stock_Exchange") %>% 
                              html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[2]') %>% #table containing the information
                              html_table()) #retrieve table
 
@@ -33,16 +32,17 @@ all.firms =
   filter(First.day.of.listing <= "2019-12-31")
 
 # Were any companies delisted?
-url2 = "https://en.wikipedia.org/wiki/List_of_companies_delisted_from_Oslo_Stock_Exchange"
-delisted.firms = as.data.frame(read_html(url2) %>% 
+delisted.firms = as.data.frame(read_html("https://en.wikipedia.org/wiki/List_of_companies_delisted_from_Oslo_Stock_Exchange") %>% 
                                  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[2]') %>% #table containing the information
                                  html_table()) #retrieve table
 
 
 inner_join(all.firms,delisted.firms,by = "Company")
 
-# Seems like only Elkem was delisted in or after 2005, the code will automatically 
-# exclude it since the stock price info will not be available after delisting
+# Seems like only Elkem was delisted in or after 2005
+all.firms = 
+  all.firms %>% 
+  filter(Company != "Elkem")
 
 # Get tickers
 
@@ -81,9 +81,27 @@ all.firms$Company =
   gsub("Scatec Solar","Scatec",.) %>% 
   gsub("Lerøy Seafood Group","Lerøy",.) %>% 
   gsub("Gjensidige Forsikring","Gjensidige",.) %>% 
+  gsub("InterOil Exploration and Production","InterOil",.) %>% 
+  gsub("Orkla Group","Orkla",.) %>% 
+  gsub("Oceanteam Shipping", "Oceanteam") %>% 
+  gsub("Solon Eiendom", "Solon") %>% 
+  gsub(" International","",.) %>% 
+  gsub(" Ltd","",.) %>% 
   gsub("\\*","",.) %>% 
   gsub("\\.","",.) %>% 
-  gsub(" ASA","",.)
+  gsub(" ASA","",.) %>% 
+  gsub(" Limited","")
+
+# Remove company names that sound too similar (searching for "Aker" will give
+# results of "Aker BP" and "Aker Solutions" for example, "Wilh Wilhelmsen Holding"
+# has both "ser A" and "ser B")
+
+all.firms = 
+  all.firms %>% 
+  subset(Company != "Aker" & Company != "Odfjell ser A" &
+         Company != "Odfjell ser B" & Company != "Schibsted ser A" & 
+         Company != "Schibsted ser B" & Company != "Wilh Wilhelmsen Holding ser A" &
+         Company != "Wilh Wilhelmsen Holding ser B")
 
 # Edit ticker to be on the format "TICKER.OL"
 all.firms$ticker = paste0((gsub("OSE: ","",all.firms$Ticker)),".OL")
@@ -105,10 +123,11 @@ all.stocks <- BatchGetSymbols(tickers = all.tickers,
                               do.cache = FALSE
 )
 
-# How many companies do we have? (95)
+# How many companies do we have? (126)
 # We originally had 183 tickers for companies, some only had price info for
 # <75% of the time period (and was therefore skipped), some tickers didn't have
-# any info (deregistered or acquired by other companies and therefore no info)
+# any info (deregistered or acquired by other companies and therefore no info),
+# some we removed to make searching easier later)
 sum(ifelse(all.stocks$df.control$threshold.decision=="KEEP",1,0))
 
 # Convert stock information into a data frame
@@ -118,7 +137,7 @@ stocks = all.stocks$df.tickers
 stocks = left_join(stocks,all.firms,by="ticker")
 
 # Calculate a daily price measure
-for(i in 1:length(stocks)){
+for(i in 1:nrow(stocks)){
   stocks$av.price[i] = (stocks$price.open[i]+stocks$price.close[i])/2
 }
 
@@ -127,8 +146,9 @@ stocks =
   stocks %>% 
   select(Company,ticker,"date"=ref.date,av.price)
 
+save(stocks,file="stocks.Rdata")
 
-
+##############################################################################
 
 
 # NEWS ARTICLE RETRIEVAL 
@@ -171,6 +191,7 @@ date.list <- date.list[-c(500, 1015, 1016, 4709, 4728, 4819, 5290, 5551, 5655, 5
 
 # Log in to DN subscription
 # Only run after having closed R/cleaned environment!
+
 # url <- "https://www.dn.no/auth/login"
 # uastring <- "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
 # session <- session(url, user_agent(uastring))
@@ -193,9 +214,31 @@ date.list <- date.list[-c(500, 1015, 1016, 4709, 4728, 4819, 5290, 5551, 5655, 5
 #   text <- rbind(text, toString(html))
 # }
 
+#url <- "https://www.dn.no/auth/login"
+#uastring <- "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+#session <- session(url, user_agent(uastring))
+#form <- html_form(session)[[1]]
+#fill <- html_form_set(form, 
+#                      username = "livewt@live.no",
+#                      password = "masterthesis123")
+#session_submit(session, fill, submit = NULL, config(referer = session$url))
+
+# Extract text from each article
+#text <- list()
+
+#for (url in url.list) {
+#  jump <- session %>% 
+#    session_jump_to(url)  # Jump to each URL logged in
+#  html <- read_html(jump) %>% 
+#    html_nodes("article") %>% 
+#    html_nodes("section") %>% 
+#    html_nodes("p")
+#  text <- rbind(text, toString(html))
+#}
+
 # save(text, file = "text.RData")
 
-load("text.RData")
+# load("text.RData")
 
 # Remove HTML code and everything but letters
 text <- text %>% 
@@ -230,81 +273,239 @@ text <- text[!duplicated(text$text), ]
 
 
 
+################################################################################
 
-
-
-# CONNECT ARTICLES AND COMPANIES
+# Start here 
+rm(list = ls())
+load("text.Rdata")
+load("stocks.Rdata")
 
 # Get the names of the companies we have stock price data for
 companies = unique(stocks$Company)
 
-# Create a nice data frame for the results
-mycols = c("text","date","url",companies)
-df = data.frame(matrix(ncol = length(mycols),nrow = nrow(text)))
-colnames(df) = mycols
-df$date = text$date
-df$text = text$text
-df$url = text$url
+# Add company names more used/or changed during time period
+companies = c(companies,c("Statoil", "Marine Harvest", "Det Norske Oljeselskap",
+                          "Apptix", "Clavis Pharma", "AqualisBraemar", 
+                          "Bergen Group", "Vik Sparebank", "Aurland Sparebank",
+                          "Indre Sogn Sparebank", "Noreco", "Team Bane", 
+                          "Namsos Traffikkselskap", "PGS", "Solstad",
+                          "PSI", "Ocean Technology"))
 
+# 1. Which companies are never mentioned?
+# Create data frame with "companies" column and "mentioned" column
+comp.df = data.frame(companies,"mentioned" = 0)
 
-# An idea I had was to try to find out which companies are mentioned and which are
-# not, so we can exclude the companies that aren't mentioned at all and then search
-# for how many times the remaining companies are mentioned (maybe easier to work with
-# fewer companies), this doesn't work yet for some reason but DNB example below works
-for(i in 1:nrow(df)){
-  for(j in 1:length(companies)){
-    for(k in 4:ncol(df)){
-      df[i,k] = ifelse(str_detect(df$text[i],companies[j]),1,0)
+# Create string with text from all articles (easier to search in than in each row)
+articles = toString(text$text)   # Takes a minute or two
+
+# Loop: for i in each row of comp.df, assign 1 to the "mentioned" column if a
+# company name is found in the "articles" string and 0 if not
+for(i in 1:nrow(comp.df)){
+  comp.df$mentioned[i] = 
+    ifelse(str_detect(string = articles,
+                      pattern = comp.df$companies[i])==TRUE,
+           1,NA)    # If company i is not detected in the "articles" string ,
+}
+
+# Remove companies that are never mentioned from the "companies" list
+comp.df.new = na.omit(comp.df)
+
+# Create new list with company names that are mentioned
+companies = comp.df.new$companies
+
+# New column in text data frame for company names
+text$Company <- ""
+
+# Loop to paste company names into new Company column
+for (company in 1:length(companies)) {
+  for (t in 1:length(text$text)) {
+    if (grepl(companies[company], text[t,1], fixed = TRUE)) {
+      m <- gregexpr(companies[company], text[t,1])
+      ct <- text[t,4]
+      name <- toString(regmatches(text[t,1], m)[[1]])
+      name <- str_replace_all(name, " ", "-") 
+      text[t,4] <- paste(ct, name)
     }
   }
 }
 
+text <- text[!text$Company=="",] # Remove rows with no company names
 
-# DNB test (same concept as above but only for DNB)
-dnbcols = c("text","date","url","DNB")
-dnb.df = data.frame(matrix(ncol = length(dnbcols),nrow = nrow(text)))
-colnames(dnb.df) = dnbcols
-dnb.df$date = text$date
-dnb.df$text = text$text
-dnb.df$url = text$url
-
-dnb = c("DNB")
-for(i in 1:nrow(dnb.df)){
-  for(j in 1:length(dnb)){
-    for(k in 4:ncol(dnb.df)){
-      dnb.df[i,k] = ifelse(str_detect(dnb.df$text[i],dnb[j]),1,0)
+# Loop to remove company names that only get mentioned once per article
+for (t in 1:length(text$Company)) {
+  for (c in companies) {
+    if (str_count(text[t,4], c) <= 1) {
+      text[t,4] <- gsub(c, "", text[t,4])
     }
   }
 }
 
+# Remove rows where only companies were mentioned once, commas and spaces at the start of rows
+text$Company <- str_replace_all(text$Company, ",", " ")
 
+text$Company <- text$Company %>% 
+  gsub("^[[:space:]]+$", NA, .) %>% 
+  gsub("^[[:space:]]+", "", .)
 
+text <- text[!(is.na(text$Company)),]
 
-# Previous attempt below
-
-
-
-# Which articles have company mentions?
-# Creates an object for each article that has a company mention
-# I know this works. Also if for example DNB and Equinor are both mentioned in
-# an article, it creates 2 objects for each company.
-for(i in 1:nrow(text)){
-  for(j in 1:length(companies)){
-    ifelse(str_detect(text$text[i],companies[j]),    # If the company name j is in article i
-           assign(paste(companies[j],text$date[i],sep="-"),text$text[i]),    # Yes: Create an object with the article
-           assign(paste("NA",companies[j],text$date[i],sep="-"),text$text[i]))    # No: Create an object with the article with NA in the front
-    rm(list = ls(pattern = "^NA"))    # Delete the NA objects
+# Loop to choose the most mentioned company for each article
+for (t in 1:length(text$Company)) {
+  for (c in companies) {
+    words <- strsplit(text[t,4], "[[:space:]]+")[[1]]
+    most <- names(sort(table(words), decreasing = TRUE))[1]
+    text[t,4] <- text[t,4] %>% gsub(text[t,4], "", text[t,4]) %>% 
+      gsub("", most, text[t,4])
   }
-} 
+}
 
+# Replace - with spaces and remove spaces before company names
+text$Company <-  text$Company %>% 
+  gsub("-", " ",.) %>% 
+  gsub("^[[:space:]]+", "",.)
+
+# Change old company names to new company names 
+text$Company <- text$Company %>% 
+  gsub("Statoil", "Equinor", .) %>% 
+  gsub("Marine Harvest", "Mowi", .) %>% 
+  gsub("Det Norske Oljeselskap", "Aker BP", .) %>% 
+  gsub("Apptix", "Carasent", .) %>% 
+  gsub("Clavis Pharma|AqualisBraemar", "Aqualis", .) %>% 
+  gsub("Bergen Group", "Endúr", .) %>% 
+  gsub("'Vik Sparebank'|'Aurland Sparebank'|'Indre Sogn Sparebank'", 
+       "Sogn Sparebank", .) %>% 
+  gsub("Noreco", "Norwegian Energy Company", .) %>% 
+  gsub("Team Bane", "NRC Group", .) %>% 
+  gsub("Namsos Trafikkselskap", "NTS", .) %>% 
+  gsub("PGS", "Petroleum Geo-Services", .) %>% 
+  gsub("Solstad", "Solstad Farstad", .) %>% 
+  gsub("PSI", "StrongPoint", .) %>% 
+  gsub("Ocean Technology", "Subsea", .)
+
+# Create row with date and company to identify instances where there are more than 1
+# article about a company on a specific date:
+text$datecomp = paste(text$date, text$Company)
+
+# Select relevant columns
+text2 = text %>% 
+  select(datecomp,text,date,Company) %>% 
+  group_by(datecomp)
+
+# Split text data frame into df1 (non-duplicated rows) and d1b (duplicated rows)
+df1 = text2[!duplicated(text2$datecomp),]
+df1b = text2[duplicated(text2$datecomp),]
+
+# Check that all rows have been assigned to either d1 or d1b
+nrow(df1)+nrow(df1b)-nrow(text)
+
+# Split duplicated rows (d1fb) into df2 (non-duplicated rows) and df2b (duplicated rows)
+# This is done to take into account 2 articles per company per day
+df2 = df1b[!duplicated(df1b$datecomp),]
+df2b = df1b[duplicated(df1b$datecomp),]
+
+# Combine df1 (non-duplicated rows of 1 article per day) with df2 (non-duplicated
+# rows of 2 articles per day)
+df12 = full_join(df1,df2,by="datecomp")
+
+# Split duplicated rows (df2b) into df3 (non-duplicated rows) and df3b (duplicated rows)
+# This is done to take into account 3 articles per company per day
+df3 = df2b[!duplicated(df2b$datecomp),]
+df3b = df2b[duplicated(df2b$datecomp),]
+
+# Combine df12 (non-duplicated rows of 1 or 2 articles per day) with df3 (non-duplicated
+# rows of 3 articles per day)
+df23 = full_join(df12,df3,by="datecomp")
+
+# Split duplicated rows (df3b) into df4 (non-duplicated rows) and df4b (duplicated rows)
+# This is done to take into account 4 articles per company per day
+df4 = df3b[!duplicated(df3b$datecomp),]
+df4b = df3b[duplicated(df3b$datecomp),]
+
+# Combine df23 (non-duplicated rows of 1, 2 or 3 articles per day) with df4 (non-duplicated
+# rows of 4 articles per day)
+df34 = full_join(df23,df4,by="datecomp")
+
+# Split duplicated rows (df4b) into df5 (non-duplicated rows) and df5b (duplicated rows)
+# This is done to take into account 5 articles per company per day
+df5 = df4b[!duplicated(df4b$datecomp),]
+df5b = df4b[duplicated(df4b$datecomp),]
+
+# Combine df34 (non-duplicated rows of 1, 2, 3 or 4 articles per day) with df5 (non-duplicated
+# rows of 5 articles per day)
+df45 = full_join(df34,df5,by="datecomp")
+
+# Split duplicated rows (df5b) into df6 (non-duplicated rows) and df6b (duplicated rows)
+# This is done to take into account 6 articles per company per day
+df6 = df5b[!duplicated(df5b$datecomp),]
+df6b = df5b[duplicated(df5b$datecomp),]
+
+# Combine df45 (non-duplicated rows of 1, 2, 3, 4 or 5 articles per day) with df6 (non-duplicated
+# rows of 6 articles per day)
+df56 = full_join(df45,df6,by="datecomp")
+
+
+# Split duplicated rows (df6b) into df7 (non-duplicated rows) and df7b (duplicated rows)
+# This is done to take into account 7 articles per company per day
+df7 = df6b[!duplicated(df6b$datecomp),]
+df7b = df6b[duplicated(df6b$datecomp),]
+
+# Combine df56 (non-duplicated rows of 1, 2, 3, 4, 5 or 6 articles per day) with df7 (non-duplicated
+# rows of 7 articles per day)
+df67 = full_join(df56,df7,by="datecomp")
+
+# Split duplicated rows (df7b) into df8 (non-duplicated rows) and df8b (duplicated rows)
+# This is done to take into account 8 articles per company per day
+df8 = df7b[!duplicated(df7b$datecomp),]
+df8b = df7b[duplicated(df7b$datecomp),]
+
+# Combine df67 (non-duplicated rows of 1, 2, 3, 4, 5, 6 or 7 articles per day) with df8 (non-duplicated
+# rows of 8 articles per day)
+df78 = full_join(df67,df8,by="datecomp")
+
+# Split duplicated rows (df8b) into df9 (non-duplicated rows) and df9b (duplicated rows)
+# This is done to take into account 9 articles per company per day
+df9 = df8b[!duplicated(df8b$datecomp),]
+df9b = df8b[duplicated(df8b$datecomp),]
+
+# Combine df78 (non-duplicated rows of 1, 2, 3, 4, 5, 6, 7 or 8 articles per day) with df9 (non-duplicated
+# rows of 9 articles per day)
+df89 = full_join(df78,df9,by="datecomp")
+
+# Are there still duplicate dates?
+length(which(duplicated(df9b$datecomp)==TRUE))    # We have reached the end
+
+# Combine df89 (non-duplicated rows of 1,2,3,4,5,6,7,8 or 9 articles per day)
+# with df9b (final non-duplicated rows)
+df.end = full_join(df89,df9b,by="datecomp")
+
+# Replace NAs with " " (tried a few methods, nothing works)
+#df.end = str_replace(string = df.end,
+#                     pattern = "NA",
+#                     replacement = " ")
+
+# Combine columns (note that combining without changing NA columns results in some
+# elements in the text-column end with "NA" or "NANANANANA" or something similar)
+df.end = 
+  df.end %>% 
+  mutate("text" = 
+           paste0(text.x,text.y,text.x.x,text.y.y,text.x.x.x,text.y.y.y,
+                  text.x.x.x.x,text.y.y.y.y,text.x.x.x.x.x,text.y.y.y.y.y,
+                  sep=" ")) %>% 
+  select(datecomp,text,"date" = date.x,"Company" = Company.x)
+
+# Merge df.end with stocks by date and company
+end.df <- merge(df.end, stocks, by=c("date","Company")) # Merge text and stocks df's
+
+# Remove all my unnecessary dfs hahaha :)
+rm(list = ls(pattern = "^df"))
+
+###############################################################################
 
 # Code to retrieve stopwords with package "stopwords"
 stopwords(language = "no")
 
 
-
-
-
+###############################################################################
 
 
 # TRANSLATION OF LOUGHRAN AND MCDONALD DICTIONARY
